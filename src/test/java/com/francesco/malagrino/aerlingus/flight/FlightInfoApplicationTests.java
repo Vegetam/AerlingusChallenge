bb4187763b34fcabc96d50a6aaefbe16a239bc10
Francesco Malagrino <francesco.malagrino@gmail.com>
2020-11-22 14:00:02 +0100

    Project Done
    Project Done

---
+++ src/test/java/com/francesco/malagrino/aerlingus/flight/FlightInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francesco.malagrino.aerlingus.flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
